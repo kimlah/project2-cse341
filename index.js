6ae7cd78a9c3ccfa125d63cf8e43e68bfa03d4d0
kimlah <71352287+kimlah@users.noreply.github.com>
2023-02-24 20:44:03 -0800

    attempt at auth0

--- index.js
+++ index.js
@@ -1,20 +1,54 @@
+const dotenv = require('dotenv');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const { expressjwt: jwt } = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
 
 const port = process.env.PORT || 8081;
 const app = express();
+const baseUrl = process.env.AUTH0_BASE_URL;
+const issuerBaseUrl = process.env.AUTH0_ISSUER_BASE_URL;
+const audience = process.env.AUTH0_AUDIENCE;
+
+if (!baseUrl || !issuerBaseUrl) {
+  throw new Error('Please make sure that the file .env.local is in place and populated');
+}
+
+if (!audience) {
+  console.log('AUTH0_AUDIENCE not set in .env.local. Shutting down API server.');
+  process.exit(1);
+}
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors({ origin: baseUrl }));
+
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `${issuerBaseUrl}/.well-known/jwks.json`
+  }),
+  audience: audience,
+  issuer: `${issuerBaseUrl}/`,
+  algorithms: ['RS256']
+});
+
+app.get('/api/shows', checkJwt, (req, res) => {
+  res.send({
+    msg: 'Your access token was successfully validated!'
+  });
+});
 
 app
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://cse341-contacts-frontend.netlify.app/');
-    //res.setHeader(
-    //  'Access-Control-Allow-Origin',
-    //  'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
-    //);
-    //res.setHeader('Content-Type', 'text/html');
-    //res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
   })
   .use('/', require('./routes'));
@@ -27,3 +61,5 @@ mongodb.initDb((err) => {
     console.log(`Connected to DB and listening on ${port}`);
   }
 });
+
+process.on('SIGINT', () => server.close());
