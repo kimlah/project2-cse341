06963f2bd6f3cdb7ceb618348089607e2dbc7fd8
kimlah <71352287+kimlah@users.noreply.github.com>
2023-02-25 11:19:17 -0800

    new attempt, env variables added in render

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
+require('dotenv').config();
+const axios = require('axios');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
+const path = require('path');
 
 const port = process.env.PORT || 8081;
 const app = express();
+
+app.use(express.static('static'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/static/index.html'));
+});
+
+app.get('/auth', (req, res) => {
+  res.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`,
+  );
+});
+
+app.get('/oauth-callback', ({ query: { code } }, res) => {
+  const body = {
+    client_id: process.env.GITHUB_CLIENT_ID,
+    client_secret: process.env.GITHUB_SECRET,
+    code,
+  };
+  const opts = { headers: { accept: 'application/json' } };
+  axios
+    .post('https://github.com/login/oauth/access_token', body, opts)
+    .then((_res) => _res.data.access_token)
+    .then((token) => {
+      // eslint-disable-next-line no-console
+      console.log('My token:', token);
+
+      res.redirect(`/?token=${token}`);
+    })
+    .catch((err) => res.status(500).json({ err: err.message }));
+});
 
 app
   .use(bodyParser.json())
@@ -21,3 +55,4 @@ mongodb.initDb((err) => {
     console.log(`Connected to DB and listening on ${port}`);
   }
 });
+
